dd075c1feb31ca457e622f36964bb2140db7e995
dsuru1499 <dsuru5244@gmail.com>
2019-07-22 11:59:36 +0200

    [DSU] deployment http://siri-demo-siri-demo.apps.us-east-1.online-starter.openshift.com

--- src/services/LinesDiscoveryService.js
+++ src/services/LinesDiscoveryService.js
@@ -2,7 +2,7 @@ const LinesDiscoveryService = {
     get: function (options) {
         let url = (process.env.NODE_ENV !== "production") ? "http://127.0.0.1:8080" : ""
         url += "/siri-lite/lines-discovery";
-        url += options && Object.entries(options).map(([key, value]) => `${key}=${value}`).join('&');
+        url += options && "?" + Object.entries(options).map(([key, value]) => `${key}=${value}`).join('&');
         return fetch(url, {
             "Content-Type": "application/json"
         }).then(response => response.json());
--- src/services/StopPointsDiscoveryService.js
+++ src/services/StopPointsDiscoveryService.js
@@ -2,7 +2,7 @@ const StopPointsDiscoveryService = {
     get: function (options) {
         let url = (process.env.NODE_ENV !== "production") ? "http://127.0.0.1:8080" : ""
         url += "/siri-lite/stop-points-discovery";
-        url += options && Object.entries(options).map(([key, value]) => `${key}=${value}`).join('&');
+        url += options && "?" + Object.entries(options).map(([key, value]) => `${key}=${value}`).join('&');
         return fetch(url, {
             "Content-Type": "application/json"
         }).then(response => response.json());
